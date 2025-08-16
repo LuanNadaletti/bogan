85569c02292e2a42a2a14d6e39b5df73f7282db5
Luan Nadaletti <luannadaletti6@gmail.com>
2025-08-16 17:47:30 -0300

    fix error in resolving bean provider instance

--- src/main/java/br/com/bogan/resolve/injection/ProviderInjectionAdapter.java
+++ src/main/java/br/com/bogan/resolve/injection/ProviderInjectionAdapter.java
@@ -1,9 +1,11 @@
 package br.com.bogan.resolve.injection;
 
+import br.com.bogan.definition.InjectionPoint;
 import br.com.bogan.error.NoSuchComponentException;
+import br.com.bogan.factory.ComponentFactory;
 import br.com.bogan.provider.Provider;
 import br.com.bogan.resolve.ResolverContext;
-import br.com.bogan.definition.InjectionPoint;
+import br.com.bogan.util.ComponentNameUtils;
 
 public class ProviderInjectionAdapter implements SpecialInjectionAdapter {
 
@@ -14,13 +16,20 @@ public class ProviderInjectionAdapter implements SpecialInjectionAdapter {
 
     @Override
     public Object resolve(InjectionPoint p, ResolverContext ctx) {
-        Object candidate = ctx.factory().getComponent(p.getTypeArgument(), p.getQualifier(), ctx);
+        final ComponentFactory factory = ctx.factory();
+        final Class<?> type = p.getTypeArgument();
+        final String qualifier = p.getQualifier();
+        final boolean required = p.isRequired();
 
-        if (p.isRequired() && candidate == null) {
-            throw new NoSuchComponentException(p.getMemberName());
-        }
-
-        return (Provider<Object>) () -> candidate;
+        return (Provider<Object>) () -> {
+            Object bean = factory.getComponent(type, qualifier, ctx);
+            if (bean == null && required) {
+                throw new NoSuchComponentException(
+                        ComponentNameUtils.formatNameAndQualifier(type, qualifier)
+                );
+            }
+            return bean;
+        };
     }
 
     @Override
